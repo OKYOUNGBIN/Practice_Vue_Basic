21a412857fcac58de1fb8ce74f387fca0791111d
youngbin <redmangos@naver.com>
2022-03-23 21:56:10 +0900

    if/ else

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div>
     <h1>Hello Vue</h1>
-    <h2 class="line-through">line-through</h2>
-    <h2 v-bind:class="textDecoration" class="text-red">line-through</h2>
-    <h2 :class="isDone === true ? 'line-through' : 'highlight'">
-      line-through
-    </h2>
-    <h2
-      :class="{
-        highlight: isDone === false,
-        'text-red': username === 'scalper',
-      }"
-    >
-      Object형태의 동적 클래스
-    </h2>
-    <h2
-      :class="[
-        isDone === true ? 'line-through' : 'highlight',
-        username === 'scalper' ? 'text-red' : 'text-green',
-      ]"
-    >
-      Array 형태의 동적 클래스 부여
-    </h2>
+    <!-- <h2 v-if="showName">My name is {{ user.name }}</h2>
+    <h2 v-else>이름을 보여 줄 수 없습니다</h2>
+    <h2 v-if="user.age > 20">당신은 성인 입니다.</h2>
+    <h2 v-else-if="user.age > 14 && user.age < 20">당신은 청소년 입니다.</h2>
+    <h2 v-else>당신은 어린이 입니다.</h2> -->
+    <h2 v-if="!showName">{{ user.name }} IF</h2>
+    <h2 v-show="!showName">{{ user.name }} Show</h2>
+    <ul>
+      <template v-if="question === 'frontend'">
+        <li>HTML은 재밌나요?</li>
+        <li>CSS 재밌나요?</li>
+        <li>Javascript 재밌나요?</li>
+      </template>
+      <template v-else>
+        <li>Java 재밌나요?</li>
+        <li>Python 재밌나요?</li>
+        <li>C# 재밌나요?</li>
+      </template>
+    </ul>
   </div>
 </template>
 
@@ -30,26 +28,16 @@ export default {
   name: "App",
   data() {
     return {
-      username: "code",
-      isDone: true,
-      textDecoration: "line-through",
+      question: "frontend",
+      showName: true,
+      user: {
+        name: "scalper",
+        age: 15,
+        job: "programmer",
+      },
     };
   },
 };
 </script>
 
-<style>
-.text-red {
-  color: red;
-}
-.text-green {
-  color: green;
-}
-.highlight {
-  font-weight: bold;
-  background: pink;
-}
-.line-through {
-  text-decoration: line-through;
-}
-</style>
+<style></style>
