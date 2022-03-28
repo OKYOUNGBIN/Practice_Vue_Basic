992880e3e83c24fdf264772df2c31cbad09d8d46
youngbin <redmangos@naver.com>
2022-03-28 20:52:19 +0900

    form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
   <div>
-    <h1>Hello Vue {{ name }}</h1>
-    <button v-on:click.right="changeName">change Name</button>
-    <button v-on:mouseover="name = 'Code Scalper'">change Name</button>
-    <button v-on:mouseleave="name = 'scalper'">change Name</button>
-    <a v-on:click.prevent="movePage" href="https://naver.com">naver로 이동</a>
-    <h2>{{ number }}</h2>
-    <button v-on:click="increment(1)">숫자 1증가</button>
-    <button v-on:click="decrement(1)">숫자 1감소</button>
+    {{ user }}
+    <h1>{{ user.name }}</h1>
+    <form>
+      <div>
+        <label for="name">이름</label>
+        <input type="text" id="name" v-model="user.name" @input="setValue" />
+      </div>
+      <div>
+        <label for="age">나이</label>
+        <input type="text" id="age" v-model="user.age" />
+      </div>
+      <div>
+        <label for="city">사는곳</label>
+        <select id="city" v-model="user.city">
+          <option value="seoul">서울</option>
+          <option value="daegu">대구</option>
+          <option value="busan">부산</option>
+          <option value="daejeon">대전</option>
+          <option value="ulsan">울산</option>
+        </select>
+      </div>
+      <div>
+        <label for="favorite-food">좋아하는 음식</label>
+        <select id="favorite-food" multiple v-model="user.favorite">
+          <option
+            v-for="option in foodOptions"
+            :value="option.code"
+            :key="option.code"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label for="job">직업</label>
+        프로그래머<input
+          type="checkbox"
+          value="programmer"
+          v-model="user.job"
+        />
+        가수<input type="checkbox" value="singer" v-model="user.job" />
+        교사<input type="checkbox" value="teacher" v-model="user.job" />
+      </div>
+      <div>
+        <label for="job">성별</label>
+        <input type="radio" value="male" v-model="user.gender" />
+        <input type="radio" value="female" v-model="user.gender" />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -16,37 +57,36 @@ export default {
   name: "App",
   data() {
     return {
-      name: "scapler",
-      number: 0,
+      foodOptions: [
+        { label: "짜장면", code: "JJ" },
+        { label: "짬뽕", code: "JB" },
+        { label: "탕수육", code: "TS" },
+      ],
+      user: {
+        name: "",
+        age: 0,
+        city: "seoul",
+        favorite: [],
+        job: [],
+      },
     };
   },
   methods: {
-    changeName() {
-      this.name = "Code Scalper";
-    },
-    movePage(event) {
-      //
-      event.preventDefault();
-      const check = confirm("페이지를 이동하시겠습니까?");
-      if (check) {
-        console.log("page이동");
-      } else {
-        console.log("페이지 이동 X");
-      }
-    },
-    increment(num) {
-      this.number += num;
-    },
-    decrement(num) {
-      this.number -= num;
+    setValue(e) {
+      console.log(e.target.value);
+      this.user.name = e.target.value;
     },
   },
 };
 </script>
 
 <style>
-a {
-  font-size: 24px;
-  display: block;
+label {
+  font-size: 22px;
+  font-weight: bold;
+  margin-right: 1rem;
+}
+div {
+  margin-bottom: 1rem;
 }
 </style>
