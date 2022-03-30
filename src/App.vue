a879dd2d25ea26a62dbb2e0cfd6a1229be643508
youngbin <redmangos@naver.com>
2022-03-30 21:29:19 +0900

    component2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,37 @@
 <template>
   <div>
-    <h2>Current Money :: {{ money }}</h2>
-    <div>
-      <button @click="money += 100">earn money</button>
-      <button @click="money -= 100">spend money</button>
-    </div>
-    <h3>{{ receit }}</h3>
-    <button @click="receit.food += 500">buy food</button>
-    <hr />
-    <input type="text" v-model="userName" />
+    <GreetingUser
+      :username="username"
+      :numberOfVisit="numberOfVisit"
+      :siteInfo="siteInfo"
+    />
+
+    <button @click="changeName()">change</button>
   </div>
 </template>
 
 <script>
+import GreetingUser from "./components/GreetingUser.vue";
 export default {
   name: "App",
+  components: {
+    GreetingUser,
+  },
   data() {
     return {
-      userName: "scalper",
-      money: 0,
-      receit: {
-        food: 3000,
-        fee: 2000,
-        fare: 10000,
-      },
+      username: "scapler",
+      numberOfVisit: undefined,
+      siteInfo: undefined,
     };
   },
   computed: {},
-  watch: {
-    userName: {
-      handler(newValue) {
-        console.log(newValue, "newValue");
-      },
-      immediate: true,
-    },
-    // receit(newValue, oldValue) {
-    //   console.log("영수증에 값 변화가 있음", newValue, oldValue);
-    // },
-    receit: {
-      handler(newValue) {
-        console.log("영수증에 값 변화", newValue);
-      },
-      deep: true,
-    },
-    money(newValue, oldValue) {
-      console.log(oldValue);
-      if (newValue > 2000 && newValue > oldValue) {
-        console.log("mission complete");
-      }
-      if (oldValue < 1500 && newValue < oldValue) {
-        console.log("save money");
-      }
+  watch: {},
+  directives: {},
+  methods: {
+    changeName() {
+      this.username = "DDochi";
     },
   },
-  directives: {},
-  methods: {},
 };
 </script>
 
---
+++ src/components/GreetingUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Hello {{ username }}</h2>
+  <h3>오늘이 {{ numberOfVisit }} 번째 방문입니다.</h3>
+  <h3>사이트 이름은 {{ siteInfo.name }} 입니다</h3>
+</template>
+
+<script>
+export default {
+  name: "GreetingUser",
+  props: {
+    username: String,
+    numberOfVisit: {
+      type: Number,
+      default: 0,
+    },
+    siteInfo: {
+      type: Object,
+      default: () => {
+        return { name: "-", teacher: "-" };
+      },
+    },
+  },
+};
+</script>
+
+<style scoped></style>
