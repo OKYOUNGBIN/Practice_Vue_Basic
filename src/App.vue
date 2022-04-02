fb13a666e65f48fead488ebe010ef8373694353a
youngbin <redmangos@naver.com>
2022-04-02 22:06:16 +0900

    component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <GreetingUser id="greeting" />
+    <button @click="displayDetail = true">show detail</button>
+    <DetailView
+      v-if="displayDetail"
+      @closeDetail="close"
+      @sendData="showData"
+    />
   </div>
 </template>
 
 <script>
-import GreetingUser from "./components/GreetingUser.vue";
+import DetailView from "./components/DetailView.vue";
 export default {
   name: "App",
   components: {
-    GreetingUser,
-    // ProductList,
+    DetailView,
   },
   data() {
     return {
-      // products: [
-      //   { id: 0, name: "TV", price: 500000, company: "LG" },
-      //   { id: 1, name: "전자렌지", price: 900000, company: "삼성" },
-      //   { id: 2, name: "냉장고", price: 400000, company: "SK" },
-      //   { id: 3, name: "에어컨", price: 200000, company: "인텔" },
-      //   { id: 4, name: "오븐", price: 10000, company: "마아크로" },
-      // ],
+      displayDetail: false,
     };
   },
   computed: {},
   watch: {},
   directives: {},
-  methods: {},
+  methods: {
+    close() {
+      this.displayDetail = false;
+    },
+    showData(data) {
+      console.log(data, "data");
+    },
+  },
 };
 </script>
 
