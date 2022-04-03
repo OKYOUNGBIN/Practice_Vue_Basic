c7d5edf270400a065c5a5304c573bb895b755aeb
youngbin <redmangos@naver.com>
2022-04-03 14:07:41 +0900

    component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-    <button @click="displayDetail = true">show detail</button>
-    <DetailView
-      v-if="displayDetail"
-      @closeDetail="close"
-      @sendData="showData"
-    />
+    <h2>Hello Component</h2>
+    <button @click="activeTab = 'Menu1'">Menu1</button>
+    <button @click="activeTab = 'Menu2'">Menu2</button>
+    <button @click="activeTab = 'Menu3'">Menu3</button>
+    <keep-alive>
+      <component :is="activeTab"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import DetailView from "./components/DetailView.vue";
+import Menu1 from "./components/tabItems/Menu1.vue";
+import Menu2 from "./components/tabItems/Menu2.vue";
+import Menu3 from "./components/tabItems/Menu3.vue";
 export default {
   name: "App",
-  components: {
-    DetailView,
-  },
+  components: { Menu1, Menu2, Menu3 },
   data() {
     return {
-      displayDetail: false,
+      username: "scapler",
+      activeTab: "Menu1",
+    };
+  },
+  provide() {
+    return {
+      name: this.username,
     };
   },
   computed: {},
   watch: {},
   directives: {},
-  methods: {
-    close() {
-      this.displayDetail = false;
-    },
-    showData(data) {
-      console.log(data, "data");
-    },
-  },
+  methods: {},
 };
 </script>
 
