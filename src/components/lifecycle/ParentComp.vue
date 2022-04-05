723858d3b1d9a1fcf751ca6c40b4f448ac4300d7
youngbin <redmangos@naver.com>
2022-04-05 20:55:43 +0900

    life cycle

---
+++ src/components/lifecycle/ParentComp.vue
@@ -0,0 +1,46 @@
+<template>
+  <h3>Parent Component</h3>
+  <ChildComp v-if="showChild" />
+  <button @click="showChild = false">unmount child</button>
+</template>
+
+<script>
+import ChildComp from "./ChildComp.vue";
+export default {
+  name: "parentComp",
+  data() {
+    return {
+      showChild: true,
+    };
+  },
+  components: {
+    ChildComp,
+  },
+  beforeCreate() {
+    console.log("parent before create");
+  },
+  created() {
+    console.log("parent created");
+  },
+  beforeMount() {
+    console.log("parent beforeMount");
+  },
+  mounted() {
+    console.log("parent mounted");
+  },
+  beforeUpdate() {
+    console.log("parent beforeUpdate");
+  },
+  updated() {
+    console.log("parent updated");
+  },
+  beforeUnmount() {
+    console.log("parent beforeUnmount");
+  },
+  unmounted() {
+    console.log("parent unmounted");
+  },
+};
+</script>
+
+<style></style>
