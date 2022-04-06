9d8de75912a3be3a82181d7fb7ebd80b1e32bfc4
youngbin <redmangos@naver.com>
2022-04-06 21:18:53 +0900

    composition

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
     <h2>Hello Mixins</h2>
-    <ProductStatus />
-    <FeeStatus />
-    <SavingStatus />
   </div>
+  <TestComponet />
 </template>
 
 <script>
-import SavingStatus from "./components/mixin/SavingStatus.vue";
-import FeeStatus from "./components/mixin/FeeStatus.vue";
-import ProductStatus from "./components/mixin/ProductStatus.vue";
+import TestComponet from "./components/composition/TestComponent.vue";
 export default {
   name: "App",
-  components: { ProductStatus, SavingStatus, FeeStatus },
+  components: { TestComponet },
   data() {
     return {};
   },
---
+++ src/components/composition/TestComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Handsome?{{ isHandsome }}</h2>
+  <h3>{{ name }}</h3>
+</template>
+
+<script>
+import { reactive, ref, toRefs } from "vue";
+//import { reactive, toRefs } from "vue";
+export default {
+  name: "TestComponet",
+  setup() {
+    let isHandsome = ref(false);
+    const state = reactive({
+      name: "scalper",
+      age: 50,
+      job: "programmer",
+    });
+
+    return {
+      isHandsome,
+      ...toRefs(state),
+    };
+  },
+
+  //   data() {
+  //     return {
+  //       username: "scalper",
+  //     };
+  //   },
+};
+</script>
+
+<style></style>
